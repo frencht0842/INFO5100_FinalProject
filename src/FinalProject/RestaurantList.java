9fcc6e15421ccc59791b457f1e9264c8d849be56
Suneel Rahman <srahman5@umd.edu>
2024-11-07 17:58:15 -0500

    add restaurantList class vars and functions

--- src/FinalProject/RestaurantList.java
+++ src/FinalProject/RestaurantList.java
@@ -1,5 +1,37 @@
 package FinalProject;
 
+import java.util.ArrayList;
+
 public class RestaurantList {
-  String name;
+  private String name;
+  private String location;
+  private ArrayList<Object> restaurants;
+  
+  // Constructor, name only
+  public RestaurantList(String name) {
+	  this.name = name;
+  }
+  
+  // Constructor, name & list of restaurants
+  public RestaurantList(String name, ArrayList<Object> restaurants) {
+	  this.name = name;
+	  this.restaurants = restaurants;
+  }
+  
+  public ArrayList<Object> getRestaurants() {
+	  return this.restaurants;
+	  
+  }
+  
+  public void addRestaurant(Object restaurant) {
+	  if(!this.restaurants.contains(restaurant)) {
+		  this.restaurants.add(restaurant);
+	  }	 else {
+		  System.out.println(this.name + " already contains this restaurant.");
+	  }
+  }
+  
+  public void removeRestaurant(Object restaurant) {
+	  this.restaurants.remove(restaurant);
+  }
 }
