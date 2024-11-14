5d64c21ad45ecc8d38489270700d69c78a94549c
Kanchan7791 <kanchan7791@gmail.com>
2024-11-14 10:15:26 -0500

    changed code in main class
    After changing code in main class, it works now

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,20 +1,21 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
-import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Parent root = FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
+			primaryStage.setTitle("Restaurant Reviews");
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
@@ -25,3 +26,4 @@ public class Main extends Application {
 		launch(args);
 	}
 }
+
